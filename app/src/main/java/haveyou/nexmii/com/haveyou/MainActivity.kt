e73536726bcb2561d9652fdacd945b694ac24c6e
David-Rajchenberg <david.rajchenberg@fiverr.com>
2021-06-20 08:30:06 +0300

    1) Added attrs file 2) Worked on styles file 3) Addded first color 4) Will add colors through the colors-styles-attrs approach

--- app/src/main/java/haveyou/nexmii/com/haveyou/MainActivity.kt
+++ app/src/main/java/haveyou/nexmii/com/haveyou/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
  * Created By David Rajchenberg for JByte Studios
  */
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
